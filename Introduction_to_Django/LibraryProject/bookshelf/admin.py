42f160649d5bb656fbb930ef6d9fa977d7ec9ff1
ken kagai <jibken80@gmail.com>
2026-01-23 21:13:57 +0300

    Fix model registration in admin by updating to use the correct Book model and adding BookAdmin configuration

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Books
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author', 'isbn_number')    
+    list_filter = ('published_date', 'language')
 
-admin.site.register(Books)
+admin.site.register(Book, BookAdmin)
