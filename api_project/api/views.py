7c09ff2567de011d14ad79e5e5c8e71652234e3c
ken kagai <jibken80@gmail.com>
2026-02-01 21:18:56 +0300

    Add CRUD functionality for books using a ViewSet and update URLs

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# Keep your existing ListAPIView
 class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()  # Get all books
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# NEW: Full CRUD ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
