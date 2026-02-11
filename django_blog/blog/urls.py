179423fe36cc757890ee68e95dc4c35e481292f3
ken kagai <jibken80@gmail.com>
2026-02-11 20:27:30 +0300

    Refactor the url path of the comments.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,26 +1,35 @@
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, PostCreateView,
-    PostUpdateView, PostDeleteView,
-    register_view, login_view, logout_view, profile_view
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    register_view,
+    login_view,
+    logout_view,
+    profile_view,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
 )
-from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
-urlpatterns += [
+urlpatterns = [
+    # Blog posts CRUD
     path('', PostListView.as_view(), name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
+    # Comments
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
-    # authentication
+    # Authentication
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profile_view, name='profile'),
-
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -92,7 +92,7 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        post_id = self.kwargs['post_id']
+        post_id = self.kwargs['pk']
         form.instance.author = self.request.user
         form.instance.post = get_object_or_404(Post, id=post_id)
         return super().form_valid(form)
